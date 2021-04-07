265df28f33c2f81da9c1e09aa741073d93ba2deb
estelle <zeukengestelle@gmail.com>
2021-04-07 13:42:03 +0100

    7. more js concepts

--- js/main.js
+++ js/main.js
@@ -1,58 +1,139 @@
-var isPremiumUser = false;
-
-if (isPremiumUser == true) {
-	console.log("Thanks for being a loyal customer")
-
-} else{
-	console.log("You should subscribe to our premium service")
-}
 
 
-var myAge = 28;
-
-if (myAge < 1) {
-	console.log("You are a baby.")
-} else if (myAge < 3) {
-	console.log("You are a todder.")
-} else if (myAge < 10) {
-	console.log("You are a big kid")
-} else if (myAge <= 19) {
-	console.log("You are a teenager")
-} else if (myAge < 40) {
-	console.log("You are an adult")
-} else {
-	console.log("You must be old, you are not in our records")
-}
-
-
-for (var i = 0; i < 10; i++) {
-	console.log("Current i value is ", i);
-}
-
-function myFunction(){
-	console.log("My name is Estelle");
-}
-
-myFunction();
-
-
-function printName(name, age){
-	console.log("Hello ", name, " You are ", age);
-}
-
-printName("Essy", 28);
-
-
-
-var btn = document.getElementById('go-btn');
+/*var btn = document.getElementById('go-btn');
 
 function btnClicked(){
 	console.log("Button clicked!");
-	btn.removeEventListener("click", btnClicked);
-	document.getElementById("text").innerHTML = "Don't click again";
+	
+	var customText = document.getElementsByClassName("my-input");
+	var textArea = document.getElementsByClassName("my-textarea");
 
+	var results = document.getElementById("text");
+
+	console.log(textArea);
+
+	results.innerHTML = "Hello, " + customText[0].value + "<br>";
+	results.innerHTML += "Message: " + textArea[0].value;
 }
+
 
 
 
 btn.addEventListener("click", btnClicked);
+
+function saySomething(phrase){
+	console.log("You said: " + phrase);
+}
+var p = "Jesus loves you!"
+saySomething(p);
+
+
+function getPhrase(params){
+
+	var l=0;
+
+
+	if (typeof params.phrase !== "undefined") {
+		l = params.phrase.length;
+	}
+	
+	
+	if (typeof params.another !== "undefined") {
+		l += params.another.length;
+	}
+
+
+	return l;
+}
+
+var p1 = "God has a wonderful plan for you!";
+var p2 = "God is love";
+
+var phraseT = getPhrase({ another:p2});
+console.log(phraseT);
+
+
+function missinno(numbers){
+	var missing = -1;
+
+	for (var i = 0; i <= numbers.length; i++) {
+		if (numbers.indexOf(i) === -1) {
+			missing = i;
+		}
+	}
+
+	return missing
+}
+
+var numbers = [9, 1, 4, 8, 3, 7, 5, 2, 10];
+
+console.log(missinno(numbers));*/
+
+
+class Animal{
+
+	constructor(name, height, weight){
+		console.log("Animal created: ", name);
+		this.name = name;
+		this.height = height;
+		this.weight = weight;
+	}
+
+	nameLength(){
+		return this.name.length;
+	}
+
+}
+
+
+class Dog extends Animal{
+	constructor(name, height, weight, barkVolume, leashColor, speed){
+		super(name, height, weight);
+
+		this.barkVolume = barkVolume;
+		this.leashColor = leashColor;
+		this.speed = speed;
+	}
+
+	bark(){
+		if(this.barkVolume > 50){
+			console.log(this.name, "barks loudly! (Volume: ", this.barkVolume, ")");
+		} else {
+			console.log(this.name, "barks timidly. (Volume: ", this.barkVolume, ")");
+		}
+	}
+}
+
+class Fish extends Animal{
+
+	constructor(name, height, weight, swimSpeed){
+		super(name, height, weight);
+		this.swimSpeed = swimSpeed;
+	}
+
+	swim(chaserSpeed){
+		if(this.barkVolume > 50){
+			console.log(this.name, "swims around quickly. (Speed: ", this.swimSpeed, ")");
+		} else{
+			console.log(this.name, "swims around slowly. (Speed: ", this.swimSpeed, ")");
+		}
+
+		if (this.swimSpeed > chaserSpeed) {
+			console.log(this.name, "got away!");
+
+		}else {
+			console.log(this.name, "was caught");
+		}
+	}
+}
+
+var doggy = new Dog("Rex", 24, 36, 66, "purple", 52);
+
+var nemo = new Fish("Nemo", 6, 4, 26);
+
+doggy.bark();
+
+nemo.swim(doggy.speed);
+
+
+
