4dc8ee1889a56893c8a8fba41816496a819dc438
estelle <zeukengestelle@gmail.com>
2021-04-09 23:20:43 +0100

    8. Getting started with jquery

--- js/main.js
+++ js/main.js
@@ -1,139 +1,108 @@
+$(document).ready(function(){
 
 
-/*var btn = document.getElementById('go-btn');
+	$(document).on('contextmenu', function(){
+		return false;
+	});
 
-function btnClicked(){
-	console.log("Button clicked!");
-	
-	var customText = document.getElementsByClassName("my-input");
-	var textArea = document.getElementsByClassName("my-textarea");
+	$(document).on('mousedown', function(event){
+		event.preventDefault();
 
-	var results = document.getElementById("text");
+		console.log(event.which);
 
-	console.log(textArea);
+		if (event.which == 3) {
 
-	results.innerHTML = "Hello, " + customText[0].value + "<br>";
-	results.innerHTML += "Message: " + textArea[0].value;
-}
+			$('.hidden').removeClass('shown');
 
+			if ($(event.target).is('img')) {
+				$('.saveimg, .newtabs').addClass('shown');
+			} else if ($(event.target).is('a')) {
+				$('.newtabs').addClass('shown');
+			}
+			console.log(event.pageY, event.pageX);
 
+			$('#context').css({
+				top: event.pageY,
+				left: event.pageX
+			});
 
-
-btn.addEventListener("click", btnClicked);
-
-function saySomething(phrase){
-	console.log("You said: " + phrase);
-}
-var p = "Jesus loves you!"
-saySomething(p);
-
-
-function getPhrase(params){
-
-	var l=0;
-
-
-	if (typeof params.phrase !== "undefined") {
-		l = params.phrase.length;
-	}
-	
-	
-	if (typeof params.another !== "undefined") {
-		l += params.another.length;
-	}
-
-
-	return l;
-}
-
-var p1 = "God has a wonderful plan for you!";
-var p2 = "God is love";
-
-var phraseT = getPhrase({ another:p2});
-console.log(phraseT);
-
-
-function missinno(numbers){
-	var missing = -1;
-
-	for (var i = 0; i <= numbers.length; i++) {
-		if (numbers.indexOf(i) === -1) {
-			missing = i;
-		}
-	}
-
-	return missing
-}
-
-var numbers = [9, 1, 4, 8, 3, 7, 5, 2, 10];
-
-console.log(missinno(numbers));*/
-
-
-class Animal{
-
-	constructor(name, height, weight){
-		console.log("Animal created: ", name);
-		this.name = name;
-		this.height = height;
-		this.weight = weight;
-	}
-
-	nameLength(){
-		return this.name.length;
-	}
-
-}
-
-
-class Dog extends Animal{
-	constructor(name, height, weight, barkVolume, leashColor, speed){
-		super(name, height, weight);
-
-		this.barkVolume = barkVolume;
-		this.leashColor = leashColor;
-		this.speed = speed;
-	}
-
-	bark(){
-		if(this.barkVolume > 50){
-			console.log(this.name, "barks loudly! (Volume: ", this.barkVolume, ")");
-		} else {
-			console.log(this.name, "barks timidly. (Volume: ", this.barkVolume, ")");
-		}
-	}
-}
-
-class Fish extends Animal{
-
-	constructor(name, height, weight, swimSpeed){
-		super(name, height, weight);
-		this.swimSpeed = swimSpeed;
-	}
-
-	swim(chaserSpeed){
-		if(this.barkVolume > 50){
-			console.log(this.name, "swims around quickly. (Speed: ", this.swimSpeed, ")");
-		} else{
-			console.log(this.name, "swims around slowly. (Speed: ", this.swimSpeed, ")");
+			$('#context').fadeIn();
+			return false;
 		}
 
-		if (this.swimSpeed > chaserSpeed) {
-			console.log(this.name, "got away!");
+		$('#context').fadeOut();
 
-		}else {
-			console.log(this.name, "was caught");
+		/*switch(event.which){
+			case 1:
+				console.log("left click");
+				break;
+
+			case 2:
+				console.log("Middle click");
+				break;
+
+			case 3:
+				console.log("Right click");
+				break;
+		}*/
+
+	});
+
+
+	$('[data-trigger="dropdown"]').on('mouseenter', function(){
+		console.log("ok");
+		var submenu = $(this).parent().find('.submenu');
+		submenu.fadeIn(500);
+
+		$('.profile-menu').on('mouseleave', function(){
+			submenu.fadeOut(500);
+
+		});
+
+		console.log("removed");
+
+	});
+
+	$('[href="https://google.com"]').on('click', function(event){
+		console.log("Linking...");
+		event.preventDefault();
+
+	});
+
+
+	$('#prepend, #append, #replace').on('click', function(e){
+
+		var el = $(e.currentTarget);
+		var action = el.attr('id');
+		var content = $('.text').val();
+
+
+		if (action == "prepend") {
+			console.log("prepending...");
+			$('#main').prepend(content);
+		} else if (action == "append") {
+			console.log("appending...");
+			$('#main').append(content);
+		} else if (action == "replace") {
+			console.log("replacing...");
+			$('#main').html(content);
 		}
-	}
-}
 
-var doggy = new Dog("Rex", 24, 36, 66, "purple", 52);
+		$('.text').val("");
 
-var nemo = new Fish("Nemo", 6, 4, 26);
-
-doggy.bark();
-
-nemo.swim(doggy.speed);
+	});
 
 
+
+
+
+
+
+
+
+
+
+
+
+});
 
